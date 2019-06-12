8dfd1236ed5da469ffeab756b48d8bc14072f21e
Bogdan <prepelitsa.b@gmail.com>
2019-06-12 21:16:51 +0300

    task1 v2

--- task1.go
+++ task1.go
@@ -1,69 +1,33 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"log"
-	"os"
-	"os/exec"
-	"strconv"
-	"strings"
 )
 
 func main() {
-Start:
-	fmt.Print("Enter size: ")
-	inputString := ""
-	fmt.Scanln(&inputString)
-	if count, err := strconv.Atoi(inputString); err != nil {
-		fmt.Printf("Wrong input info!\n")
-		goto Start
-	} else {
-		height, width := GetSize()
-		OutPut(height, width, count)
-	}
+	width := flag.Uint64("w", 0, "set width > 0")
+	height := flag.Uint64("h", 0, "set height > 0")
+	flag.Parse()
+  flagSetCount := 0
+  flag.Visit(func(f *flag.Flag) {
+    flagSetCount++
+  })
+  if flagSetCount < 2 {
+    fmt.Println("Params should be set")
+  } else {
+	 outPut(*width, *height)
+  }
 }
 
-func GetSize() (int, int) {
-	cmd := exec.Command("stty", "size")
-	cmd.Stdin = os.Stdin
-	out, err := cmd.Output()
-	if err != nil {
-		log.Fatal(err)
-	}
-	return ConvertSizeToInt(string(out))
-}
-
-func ConvertSizeToInt(sizeOfString string) (int, int) {
-	tmp := sizeOfString[:len(sizeOfString)-1]
-	fmt.Println(tmp)
-	str := strings.Split(tmp, " ")
-	height, err := strconv.Atoi(string(str[0]))
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(2)
-	}
-	width, err := strconv.Atoi(string(str[1]))
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(2)
-	}
-	return height, width
-}
-
-func OutPut(height, width, count int) {
-	var outPutString string
-	if count > height || count+1 > width {
-		fmt.Println("Sorry, but size of this window cannot allow to write the desk")
-	} else {
-		for i := 0; i < count; i++ {
-			if i%2 != 0 {
-				outPutString += " "
-			}
-			for j := 0; j < count; j++ {
-				outPutString += " *"
-			}
-			fmt.Println(outPutString)
-			outPutString = ""
+func outPut(width, height uint64) {
+	for i := uint64(0); i < height; i++ {
+		if i%2 != 0 {
+			fmt.Printf(" ")
 		}
+		for j := uint64(0); j < width; j++ {
+      fmt.Printf("* ")
+		}
+		fmt.Printf("\n")
 	}
 }
