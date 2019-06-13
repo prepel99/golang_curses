ec320840f3f038fbc8016bc19df80972941850c2
Bogdan <prepelitsa.b@gmail.com>
2019-06-13 20:38:17 +0300

    Added help when there is no params

--- task1.go
+++ task1.go
@@ -6,17 +6,21 @@ import (
 )
 
 func main() {
-	width := flag.Uint64("w", 0, "set width > 0")
-	height := flag.Uint64("h", 0, "set height > 0")
+	width := flag.Uint64("w", 5, "set width > 0")
+	height := flag.Uint64("h", 5, "set height > 0")
 	flag.Parse()
   flagSetCount := 0
   flag.Visit(func(f *flag.Flag) {
     flagSetCount++
   })
+  if *width >uint64(100) || *height > uint64(100) {
+    fmt.Println("There is not correct one of the params")
+    panic()
+  }
   if flagSetCount == 0 {
-    fmt.Println("info")
+    fmt.Println("HELP: there is 2 flags, both must be 0 < dighit<= 100, you can set width with -w, height with -h")
   } else if flagSetCount < 2 {
-    fmt.Println("Params should be set")
+    fmt.Println("Both of params should be set correct")
   } else {
 	 outPut(*width, *height)
   }
